79c8d3b78b2c7a8ac2664cbc84d4b0c305a18a77
zhangshenglin <zhangshenglin@mail.taiji.com.cn>
2025-11-14 23:58:51 +0800

    Updates

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-// vue.config.js
-const { defineConfig } = require('@vue/cli-service')
-
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' 
-    ? '/food-chooser/'  // 重要：改成你的仓库名
-    : '/'
-})
